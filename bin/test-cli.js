c93d71dc0533ee7a1e5172f4d8362ec90e91fe11
ping.an <pingann2021@163.com>
2023-08-01 16:44:35 +0800

    update

--- bin/test-cli.js
+++ bin/test-cli.js
@@ -1,11 +1,18 @@
 #! /usr/bin/env node
 const program = require("commander");
-const pkg = require("../package.json");
+const chalk = require("chalk");
+
+// 定义命令和参数
+// create命令
 program
   .command("create <app-name>")
-  .description("create a new project")
-  .option("-f, --force", "override")
+  .description(chalk.cyan("create a new project"))
+  // -f or --force 为强制创建，如果创建的目录存在则直接覆盖
+  .option("-f, --force", "overwrite target directory if it exist")
   .action((name, options) => {
-    console.log("项目名称", name);
+    // 打印执行结果
+    require("../lib/create")(name, options);
   });
+
+// 解析用户执行命令传入参数
 program.parse(process.argv);
